9bee3b79213efd210636ced5eb07b8b71c5398bf
Rukiye Başkut <rukiyebaskut11@gmail.com>
2022-06-03 19:56:59 +0300

    03.06.2022

--- src/test/java/pages/BluerentalcarsPage.java
+++ src/test/java/pages/BluerentalcarsPage.java
@@ -1,4 +1,33 @@
 package pages;
 
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import utilities.Driver;
+
 public class BluerentalcarsPage {
+
+    public BluerentalcarsPage(){
+
+        PageFactory.initElements(Driver.getDriver(),this);
+
+    }
+
+    @FindBy(xpath = "//*[text()=' Login']")
+    public WebElement ilkLogin;
+
+    @FindBy(xpath = "//input[@id='formBasicEmail']")
+    public WebElement emailKutusu;
+
+   @FindBy(xpath = "//input[@id='formBasicPassword']")
+   public WebElement passwordKutusu;
+
+   @FindBy(xpath = "//button[@type='submit']")
+    public WebElement ikinciLogin;
+
+  @FindBy(id = "dropdown-basic-button")
+    public WebElement profilIsmi;
+
+
+
 }
--- src/test/java/tests/PozitifTest.java
+++ src/test/java/tests/PozitifTest.java
@@ -1,4 +1,46 @@
 package tests;
 
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import pages.BluerentalcarsPage;
+import utilities.ConfigReader;
+import utilities.Driver;
+
 public class PozitifTest {
+
+    BluerentalcarsPage bluerentalcarsPage = new BluerentalcarsPage();
+
+
+    @Test
+    public void pozitifLoginTest() {
+
+       //1- https://www.bluerentalcars.com/ bu url ye git
+        Driver.getDriver().get(ConfigReader.getProperty("bluerentalcarsUrl"));
+
+
+        //2- anasayfadaki login butonuna basiniz
+
+        bluerentalcarsPage.ilkLogin.click();
+
+
+        //3- login aşaasında email adress kısmına doğru maili gir "customer@bluerentalcars.com"
+       bluerentalcarsPage.emailKutusu.sendKeys(ConfigReader.getProperty("brcValidEmail"));
+
+        //4- login aşamasında password kısmına doğru şifreyi gir "12345"
+        bluerentalcarsPage.passwordKutusu.sendKeys(ConfigReader.getProperty("brcValidPassword"));
+
+        //5 -email ve şifreden sonra giriş yapabilmek için alt kısımdaki  login butonu ile giriş yap
+        bluerentalcarsPage.ikinciLogin.click();
+
+
+        //6- giriş yaptıktan sonra şifre için gelen pop'un  "kapat" bas
+        //Driver.getDriver().switchTo().alert().dismiss();
+
+        //7 -giriş yapıldıgını test etmek için  kullanınıcının isminin "Jordon Stark" oldugunu ontrol edelim
+       String expectedUsername ="Jordon Stark";
+       String actualUsername = bluerentalcarsPage.profilIsmi.getText();
+
+       Assert.assertEquals(actualUsername ,expectedUsername);
+
+    }
 }
